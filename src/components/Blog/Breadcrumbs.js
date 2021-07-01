0f3c27f40a31adc78bddba61ec8474948d85ac85
jsusilo <enr@bk.ru>
2021-07-01 18:34:21 +0500

    blog bread  comp

---
+++ src/components/Blog/Breadcrumbs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Breadcrumbs extends React.Component {
+  render() {
+    return (
+      <section className="section-34 bg-gray-light">
+        <div className="container">
+          <ul className="list-inline list-inline-12 list-inline-icon p">
+            <li className="breadcrumb-link">
+              <a href="index.html">Home</a>
+            </li>
+            <li className="breadcrumb-current">Blog</li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Breadcrumbs;
